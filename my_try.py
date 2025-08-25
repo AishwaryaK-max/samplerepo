7a5efb7f07b1234d5fc241c6f1c2f990c1bf3152
AishwaryaK-max <aishwarya2473@gmail.com>
2025-08-25 12:29:10 +0530

    feat: I added age

--- my_try.py
+++ my_try.py
@@ -1,5 +1,7 @@
 # Ask the user for their name
 name = input("Enter your name: ")
+age=int(input("Enter your age: "))
 
 # Greet the user
 print(f"Hello, {name}!, Welcome to Chennai")
+print("Your age is : ",age)
